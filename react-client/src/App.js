daa067dd920e87d1e2d50a08855655820d57d107
seetharamkoya <seetharamkoya9@gmail.com>
2023-05-27 16:25:58 +0530

    init

---
+++ react-client/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/helloworld');
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error calling API:', error);
+        setMessage('Error fetching message');
+      }
+    };
+
+    fetchHelloWorld();
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
