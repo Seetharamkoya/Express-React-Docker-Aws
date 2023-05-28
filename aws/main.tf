5b423e1d948a1dc26e82f297fd03603f34f24f06
seetharamkoya <seetharamkoya9@gmail.com>
2023-05-28 21:53:00 +0530

    updated security groups

--- aws/main.tf
+++ aws/main.tf
@@ -9,13 +9,51 @@ terraform {
     region = "eu-central-1"
   }
 }
-  
+
 resource "aws_instance" "example" {
   ami           = "ami-062f79c1d054dd995"
   instance_type = "t2.micro"
   key_name      = "Docker-aws"
+  vpc_security_group_ids = [
+    aws_security_group.re-ex.id
+  ]
 
   tags = {
     Name = "React-express-instance"
   }
 }
+
+# Create a security group 
+resource "aws_security_group" "re-ex" {
+  name = "re-ex"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
