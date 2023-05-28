3d47e891060c2ab4baaf2740b7e19ddec7298d9f
seetharamkoya <seetharamkoya9@gmail.com>
2023-05-28 19:57:25 +0530

    updated remote state

--- aws/main.tf
+++ aws/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "reactexpressec2"
+    key    = "myapp/dev/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+  
 resource "aws_instance" "example" {
   ami           = "ami-062f79c1d054dd995"
   instance_type = "t2.micro"
