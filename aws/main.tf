591454c2530f6b907fa5d2c280cefd68451b1717
seetharamkoya <seetharamkoya9@gmail.com>
2023-05-28 14:12:10 +0530

    provisioning ec2

---
+++ aws/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-062f79c1d054dd995"
+  instance_type = "t2.micro"
+  key_name      = "Docker-aws"
+
+
+  tags = {
+    Name = "React-express-instance"
+  }
+}
